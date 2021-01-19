6c6f16c78e4e4535f3f24a16d6b2b2b4b30a1dd5
Fede <chiellofederico@gmail.com>
2021-01-19 15:04:59 +0100

    fine esercizio 1

--- esercizio-1/js/script.js
+++ esercizio-1/js/script.js
@@ -1,1 +1,10 @@
 // Utilizzando const e/o let. Inserire un numero, se è pari stampa il numero, se è dispari stampa il numero successivo.
+
+// Si può utilizzare in questo caso la variabile const perché non si deve riassegnare.
+const numeroUtente = parseInt(prompt('Inserisci un numero:'));
+
+if (numeroUtente % 2 == 0) {
+  console.log('Il numero inserito è: ', numeroUtente);
+} else {
+  console.log('Se il numero è dispari viene stampato il successivo: ', numeroUtente + 1);
+}
