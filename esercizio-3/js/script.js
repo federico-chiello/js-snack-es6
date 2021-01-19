afafaf79c0ed5dc37d30f2b7b12136f9ff9399ca
Fede <chiellofederico@gmail.com>
2021-01-19 16:32:36 +0100

    esercizio 3

--- esercizio-3/js/script.js
+++ esercizio-3/js/script.js
@@ -1,6 +1,12 @@
 // Utilizzando const e/o let. Dare la possibilità di inserire due parole. Verificare se le due parole hanno la stessa lunghezza. Se hanno la stessa lunghezza, stamparle entrambe altrimenti stampare la più lunga delle due.
 
-for (let i = 0; i < 2; i++) {
-  let parola = prompt('Inserisci una parola:');
-  console.log(parola);
+const parolaUno = prompt('Inserisci una parola:');
+const parolaDue = prompt('Inserisci un\'altra parola:');
+
+if (parolaUno.length > parolaDue.length) {
+  console.log('La parola più lunga è la prima: ', parolaUno);
+} else if (parolaDue.length > parolaUno.length) {
+  console.log('La parola più lunga è la seconda: ', parolaDue);
+} else {
+  console.log('Le due parole hanno la stessa lunghezza. ' + 'Parola uno: ' + parolaUno + ' ; ' + 'parola due: ' + parolaDue);
 }
