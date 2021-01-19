33aac6a09f6cce2c20e0602b468c19baf2518bb2
Fede <chiellofederico@gmail.com>
2021-01-19 16:18:28 +0100

    aggiungo ul nell'html esercizio 2

--- esercizio-3/js/script.js
+++ esercizio-3/js/script.js
@@ -1,1 +1,6 @@
 // Utilizzando const e/o let. Dare la possibilità di inserire due parole. Verificare se le due parole hanno la stessa lunghezza. Se hanno la stessa lunghezza, stamparle entrambe altrimenti stampare la più lunga delle due.
+
+for (let i = 0; i < 2; i++) {
+  let parola = prompt('Inserisci una parola:');
+  console.log(parola);
+}
