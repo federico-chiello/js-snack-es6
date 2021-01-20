b8001a4f21afeafa10b789318437a1aeb028f55f
Fede <chiellofederico@gmail.com>
2021-01-20 15:24:52 +0100

    jsnack-1

--- jsnack-1/js/script.js
+++ jsnack-1/js/script.js
@@ -1,2 +1,32 @@
 // Creare un array di oggetti: ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
 // Stampare a schermo la bici con peso minore utilizzando destructuring e template literal.
+
+// Array di oggetti biciclette.
+const biciclette = [
+  {
+    nome : 'Fast-one',
+    peso : 300
+  },
+  {
+    nome : 'Wind',
+    peso : 230
+  },
+  {
+    nome : 'Thunder',
+    peso : 245
+  },
+  {
+    nome : 'Thunder-2',
+    peso : 260
+  },
+  {
+    nome : 'Sun',
+    peso : 280
+  }
+];
+
+// utilizziamo il metodo destructuring, cioè prendere alcune proprietà dell'oggetto e inserirle in una variabile.
+
+  const [biciUno, biciDue, biciTre, biciQuattro, biciCinque] = biciclette;
+
+  console.log(biciDue.nome + ' '+ biciDue.peso);
